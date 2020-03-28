be3759d804f62e43ab46dadb78d739e58ce4bbfc
saggins <win32prog@gmail.com>
2020-03-28 15:20:47 -0700

    Added Check for VAULT/PERMS depedency

--- economy/src/main/java/goodEconomy/GoodEconomy.java
+++ economy/src/main/java/goodEconomy/GoodEconomy.java
@@ -1,13 +1,32 @@
 package goodEconomy;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+
 public final class GoodEconomy extends JavaPlugin{
-	
+    private Economy econ;
+    private Permission perms;
+    private Chat chat;
+    
 	@Override
 	public void onEnable() {
 		log("Enabling Sagg.in Ecnonmy Mod");
+
+		if(!setupEconomy()) {
+			this.getLogger().severe("Disabled due to no Vault dependency found!");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+		  this.setupPermissions();
+	    this.setupChat();
 		
+		
+		this.getCommand("buyselleco").setExecutor(new BuySellEco());
 		log("Version " + getDescription().getVersion() + "by " + getDescription().getAuthors() + "is enabled!");
 	}
 	
@@ -20,4 +39,49 @@ public final class GoodEconomy extends JavaPlugin{
 		this.getLogger().info(msg);
 	}
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	private boolean setupEconomy() {
+        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+	
+	
+    private boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+
+    public Economy getEconomy() {
+        return econ;
+    }
+
+    public Permission getPermissions() {
+        return perms;
+    }
+
+    public Chat getChat() {
+        return chat;
+    }
 }
