b395ac8c4b82ae65e852d9fc2bf422479eef6b91
saggins <win32prog@gmail.com>
2020-03-29 15:31:56 -0700

    Cleaned Up main Method

--- economy/src/main/java/goodEconomy/GoodEconomy.java
+++ economy/src/main/java/goodEconomy/GoodEconomy.java
@@ -4,6 +4,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import goodEconomy.TabCompleter.TabMaterial;
+import goodEconomy.commands.Buy;
+import goodEconomy.commands.BuySellEco;
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.permission.Permission;
@@ -12,21 +15,21 @@ public final class GoodEconomy extends JavaPlugin{
     private Economy econ;
     private Permission perms;
     private Chat chat;
+    private static GoodEconomy INSTANCE;
     
 	@Override
 	public void onEnable() {
+        INSTANCE = this;
 		log("Enabling Sagg.in Ecnonmy Mod");
 
-		if(!setupEconomy()) {
-			this.getLogger().severe("Disabled due to no Vault dependency found!");
-            Bukkit.getPluginManager().disablePlugin(this);
-            return;
-        }
-		  this.setupPermissions();
-	    this.setupChat();
+		
+		Locales.load();
 		
 		
-		this.getCommand("buyselleco").setExecutor(new BuySellEco());
+//		this.getCommand("buyselleco").setExecutor(new BuySellEco());
+//		this.getCommand("buyz").setExecutor(new Buy());
+//		this.getCommand("buyz").setTabCompleter(new TabMaterial());;
+
 		log("Version " + getDescription().getVersion() + "by " + getDescription().getAuthors() + "is enabled!");
 	}
 	
@@ -39,49 +42,54 @@ public final class GoodEconomy extends JavaPlugin{
 		this.getLogger().info(msg);
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	private boolean setupEconomy() {
-        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            return false;
-        }
-        econ = rsp.getProvider();
-        return econ != null;
-    }
-	
-	
-    private boolean setupChat() {
-        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
-        chat = rsp.getProvider();
-        return chat != null;
+    public static GoodEconomy getInstance() {
+        return INSTANCE;
     }
 
-    private boolean setupPermissions() {
-        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
-        perms = rsp.getProvider();
-        return perms != null;
-    }
-
-    public Economy getEconomy() {
-        return econ;
-    }
-
-    public Permission getPermissions() {
-        return perms;
-    }
-
-    public Chat getChat() {
-        return chat;
-    }
+	
+	
+	
+	
+	
+	
+	
+	
+//	private boolean setupEconomy() {
+//        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
+//            return false;
+//        }
+//
+//        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+//        if (rsp == null) {
+//            return false;
+//        }
+//        econ = rsp.getProvider();
+//        return econ != null;
+//    }
+//
+//	
+//	
+//    private boolean setupChat() {
+//        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+//        chat = rsp.getProvider();
+//        return chat != null;
+//    }
+//
+//    private boolean setupPermissions() {
+//        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+//        perms = rsp.getProvider();
+//        return perms != null;
+//    }
+//
+//    public Economy getEconomy() {
+//        return econ;
+//    }
+//
+//    public Permission getPermissions() {
+//        return perms;
+//    }
+//
+//    public Chat getChat() {
+//        return chat;
+//    }
 }
